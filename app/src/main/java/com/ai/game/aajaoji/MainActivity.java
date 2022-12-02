a07a1adcd0d2091fbe5851a8ee27de34d53f6041
Bhavye Goel <bhavye.goel2002@gmail.com>
2022-12-02 18:28:43 +0530

    Initial Commit

---
+++ app/src/main/java/com/ai/game/aajaoji/MainActivity.java
@@ -0,0 +1,86 @@
+package com.ai.game.aajaoji;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.budiyev.android.codescanner.ScanMode;
+import com.google.zxing.Result;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CodeScanner scanner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int allPermissions = 1;
+        String Permissions[] = {
+                Manifest.permission.CAMERA
+        };
+
+        if(!hasPermissions(this, Permissions)){
+            ActivityCompat.requestPermissions(this, Permissions, allPermissions);
+        }
+        else{
+            runCodeScanner();
+        }
+    }
+
+    public void runCodeScanner() {
+        scanner = new CodeScanner(this, findViewById(R.id.scanner_view));
+        scanner.setAutoFocusEnabled(true);
+        scanner.setFormats(CodeScanner.ALL_FORMATS);
+        scanner.setScanMode(ScanMode.CONTINUOUS);
+
+        scanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+//                        Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), ScanResult.class);
+                        intent.putExtra("Result", result.getText());
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
+    }
+
+    public static boolean hasPermissions(Context context, String... permissions){
+        if(context != null && permissions != null){
+            for (String permission : permissions){
+                if(ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        scanner.startPreview();
+    }
+
+    @Override
+    public void onPause() {
+        scanner.releaseResources();
+        super.onPause();
+    }
+}
---
+++ app/src/main/java/com/ai/game/aajaoji/ScanResult.java
@@ -0,0 +1,25 @@
+package com.ai.game.aajaoji;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ScanResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_result);
+
+        ((TextView)findViewById(R.id.textView)).setText(getIntent().getStringExtra("Result"));
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+}
